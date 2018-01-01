f65cdfb3ebad1c98658941c472ec52c0da7ad5bb
Erick Bogarin <erick.bogarin@gmail.com>
2018-01-01 12:56:34 -0400

    Add book modeling

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,7 +1,39 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
-# Product Category
+from apps.bookshelf.models import TimestampedModel
+
+
+class Book(TimestampedModel):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(db_index=True, max_length=255, unique=True)
+
+    image = models.URLField(blank=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+
+    isbn = models.CharField(max_length=100)
+    date_published = models.DateField()
+    pages = models.IntegerField()
+
+    categories = models.ManyToManyField(
+        'Category', related_name='books',
+    )
+
+    author = models.ForeignKey(
+        'profiles.Author',
+        related_name='books',
+        on_delete=models.CASCADE
+    )
+
+    active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Book, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class Category(models.Model):
