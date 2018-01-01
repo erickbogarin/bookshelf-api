d07849a3d62e286b37754fadd365486eaacb2d69
Erick Bogarin <erick.bogarin@gmail.com>
2018-01-01 13:54:15 -0400

    Support pagination for viewsets

--- config/settings/base.py
+++ config/settings/base.py
@@ -189,4 +189,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
