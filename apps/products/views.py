a206886b70f1373f04e5646bd6fd15a2dbbcf0ca
Erick Bogarin <erick.bogarin@gmail.com>
2018-01-01 14:30:02 -0400

    Add filters on books endpoint

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from apps.products.models import Category, Book
 from apps.products.serializers import CategorySerializer, BookSerializer
@@ -8,6 +8,10 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
 
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter,)
+    search_fields = ('title', 'author__user__username')
+    ordering_fields = '__all__'
+    ordering = ('-date_published', 'title')
 
 class CategoryViewSet(viewsets.ModelViewSet):
     """
