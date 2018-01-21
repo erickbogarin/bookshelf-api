1df5cc51791a01cd4dacdb6b8b84f13832609599
Erick Bogarin <erick.bogarin@gmail.com>
2018-01-21 16:46:47 -0400

    Add docstring for UserSerializer class
    Signed-off-by: Erick Bogarin <erick.bogarin@gmail.com>

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -2,7 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """Handles serialization and deserialization of User objects."""
+
     class Meta:
         model = User
         fields = ['url', 'username', 'first_name', 'last_name', 'email', 'groups', 'is_active']
